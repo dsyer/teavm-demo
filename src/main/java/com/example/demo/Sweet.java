9d9a7bc5f5a469c6fb6474e40563b78a32c3855c
Dave Syer <dsyer@vmware.com>
2022-03-17 14:32:37 +0000

    Export 2 functions to JavaScript

--- src/main/java/com/example/demo/Sweet.java
+++ src/main/java/com/example/demo/Sweet.java
@@ -5,17 +5,35 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Sweet {
+import org.teavm.jso.JSBody;
+import org.teavm.jso.JSObject;
+
+public class Sweet implements Exported {
 
 	private static List<String> values = Arrays.asList("Hello", "World");
 
 	public static void main(String[] args) {
-		System.out.println(new Sweet().hello(args));
+		export(new Sweet());
 	}
-	
+
+	@Override
 	public String hello(String... args) {
 		List<String> values = new ArrayList<>(Sweet.values);
 		values.addAll(Arrays.asList(args));
 		return values.stream().collect(Collectors.joining(" "));
 	}
+
+	@Override
+	public void greet() {
+		System.out.println(hello());
+	}
+
+	@JSBody(params = "arg", script = "main.exported = arg;")
+	public static native void export(Exported arg);
+
+}
+
+interface Exported extends JSObject {
+	void greet();
+	String hello(String... args);
 }
