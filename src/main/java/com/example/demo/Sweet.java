0cf43fcdcd7bc6566453998583b16b5fbf7ec12e
Dave Syer <dsyer@vmware.com>
2022-03-17 13:15:28 +0000

    Use TeaVM to compile Java to JS

--- src/main/java/com/example/demo/Sweet.java
+++ src/main/java/com/example/demo/Sweet.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,8 +8,14 @@ import java.util.stream.Collectors;
 public class Sweet {
 
 	private static List<String> values = Arrays.asList("Hello", "World");
+
+	public static void main(String[] args) {
+		System.out.println(new Sweet().hello(args));
+	}
 	
-	public String hello() {
+	public String hello(String... args) {
+		List<String> values = new ArrayList<>(Sweet.values);
+		values.addAll(Arrays.asList(args));
 		return values.stream().collect(Collectors.joining(" "));
 	}
 }
