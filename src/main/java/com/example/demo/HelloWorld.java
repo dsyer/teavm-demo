69fb9f8e875a4672694f3d482b1df37e3de8b7bf
Dave Syer <dsyer@vmware.com>
2022-11-02 12:47:32 +0000

    Simple get() and echo() sample

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,100 +1,18 @@
 package com.example.demo;
 
-import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import org.teavm.interop.Export;
+import org.teavm.interop.Import;
 
-import org.apache.commons.logging.LogFactory;
-import org.springframework.util.StreamUtils;
-import org.springframework.util.StringUtils;
-import org.teavm.jso.JSBody;
-import org.teavm.jso.JSObject;
-import org.teavm.jso.core.JSMapLike;
-import org.teavm.jso.core.JSObjects;
-import org.teavm.jso.core.JSString;
-
-import am.ik.yavi.builder.ValidatorBuilder;
-import am.ik.yavi.core.ViolationMessage;
-
-public class HelloWorld implements Exports {
-
-	private am.ik.yavi.core.Validator<String> bundle;
+public class HelloWorld {
 
 	public static void main(String[] args) throws Exception {
-		export(new HelloWorld());
-		// System.out.println(Foo.class.getConstructor(String.class).newInstance("foo"));
-		LogFactory.getLog(HelloWorld.class).info("Logging: " + new Foo("foo"));
-		System.err.println(StreamUtils.copyToString(
-			HelloWorld.class.getClassLoader().getResourceAsStream("application.properties"),
-			Charset.defaultCharset()));
 	}
 
-	HelloWorld() {
-		this.bundle = ValidatorBuilder.of(String.class).constraintOnTarget(foo -> StringUtils.hasText(foo),
-				"value", ViolationMessage.of("value.notEmpty", "Empty value")).build();
+	@Export(name = "echo")
+	public static int echo() {
+		return get();
 	}
 
-	@Override
-	public String hello(String... args) {
-		return Arrays.asList(args).stream().collect(Collectors.joining(" "));
-	}
-
-	@Override
-	public void greet() {
-		System.out.println(hello("Hello", "World"));
-	}
-
-	@Override
-	public boolean validate(JSMapLike<JSObject> map) {
-		String value = JSObjects.toString(map.get("value"));
-		try {
-			return bundle.validate(value).isEmpty();
-		} catch (Throwable e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	@Override
-	public JSMapLike<JSObject> xform(JSMapLike<JSObject> map) {
-		map.set("message", JSString.valueOf(hello("Yo", "Bro")));
-		return map;
-	}
-
-
-	@JSBody(params = "arg", script = "main.exports = arg;")
-	public static native void export(Exports arg);
-}
-
-interface Exports extends JSObject {
-	void greet();
-
-	String hello(String... args);
-
-	boolean validate(JSMapLike<JSObject> map);
-
-	JSMapLike<JSObject> xform(JSMapLike<JSObject> map);
-
-}
-
-class Foo {
-	private String value;
-
-	public Foo() {}
-
-	public Foo(String string) {
-		value = string;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-
-	@Override
-	public String toString() {
-		return "Foo: value=" + value;
-	}
+	@Import(module = "env", name = "get")
+	public static native int get();
 }
